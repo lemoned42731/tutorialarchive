95b3d786355ff6c0eb92ff9f81695cc286366cc9
shigeta <shigeta@tmng.co.jp>
2022-08-01 13:59:15 +0900

    dog list tutorial

--- doglist/src/App.js
+++ doglist/src/App.js
@@ -1,25 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {fetchImages} from "./api";
+
+function Loading(){
+  return <p>Loading..</p>
+}
+function Header() {
+  return (
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Cute Dog Images</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Image(props) {
+   return (
+     <div className="card">
+       <div className="card-image">
+         <figure className="image">
+           <img src={props.src} alt="cute dog!" />
+         </figure>
+       </div>
+     </div>
+   );
+ }
+
+function Gallery(props) {
+  //const {urls} = props;
+  const urls = props.urls;
+  if(urls === null){
+    return <Loading />;
+  }
+  return (
+    <div className="columns is-vcentered is-multiline">
+        {urls.map((url)=>{
+          return(
+            <div key={url} className = "column is-3">
+              <Image src={url} />
+              </div>
+          );
+        })}
+      </div>
+  );
+}
+
+function Main() {
+  const [urls,setUrls] = useState(null);
+
+  useEffect(() =>{
+    fetchImages("shiba").then((urls) => {
+      setUrls(urls);
+    })
+  },[])
+  
+function reloadImages(breed){
+  fetchImages(breed).then((urls) => {
+    setUrls(urls)
+  })
+
+}
+
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Form onFormSubmit={reloadImages} />
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <Gallery urls={urls}/>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>Dog images are retrieved from Dog API</p>
+        <p>
+          <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
 
+function Form(props){
+  function handleSubmit(event){
+    event.preventDefault();
+    //const {breed} = event.target.elements;
+    const breed = event.target.breed;
+    props.onFormSubmit(breed.value);
+  }
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <div className="select is-fullwidth">
+              <select name="breed" defaultValue="shiba">
+                <option value="shiba">Shiba</option>
+                <option value="akita">Akita</option>
+                <option value="african">African</option>
+              </select>
+            </div>
+          </div>
+          <div className="control">
+            <button type="submit" className="button is-dark">
+              Reload
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+
+}
 export default App;
